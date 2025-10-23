47412bf0a6f593f8354d81d3fc5183476fdf2f21
Mouad <mouadharit2001@gmail.com>
2025-10-23 19:59:21 +0100

    `Removed server listener and added module export`

--- app.js
+++ app.js
@@ -36,7 +36,5 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
 
